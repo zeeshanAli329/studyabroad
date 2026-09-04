d004b2bd1a740358a2ff2d77b8bb6eb1d1d7c177
Zeeshan Ali <zeeshanali3297624@gmail.com>
2026-09-05 01:45:58 +0500

    mb-changes

---
+++ src/app/scholarship-guide/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+import next from "next";
+
+const ScholarshipGuid = () => {
+    return(
+        <>
+        <h1 className="flex mt-20 text-black">
+            Creating page holdOn
+        </h1>
+        </>
+    )
+}
+export default ScholarshipGuid;
--- src/app/scholarships/featured-scholarships/page.jsx
+++ src/app/scholarships/featured-scholarships/page.jsx
@@ -1,3 +1,13 @@
-''
+'use client'
 import next from "next";
 
+const featured = () => {
+    return(
+        <>
+        <h1 className="flex mt-20 text-black">
+            Creating page holdOn
+        </h1>
+        </>
+    )
+}
+export default featured;
